3db90298a1a0e5402e046208abfa6806fb4a5077
Alfred Neequaye <aneequaye@acsint.io>
2022-10-05 02:11:59 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,130 @@
+mod args;
+
+use args::Args;
+use image::{io::Reader, DynamicImage, ImageFormat, GenericImageView, imageops::Triangle, ImageError};
+use std::{convert::TryInto};
+
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentImageFormats,
+    BufferTooSmall,
+    UnableToReadFromPath(std::io::Error),
+    UnableToFormatImage(String),
+    UnableToDecodeImage(ImageError),
+    UnableToSaveImage(ImageError)
+}
+
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    name: String,
+}
+
+impl FloatingImage {
+    fn new(width: u32, height: u32, name: String) -> Self {
+        let buffer_capacity = height * width * 4;
+        let buffer = Vec::with_capacity(buffer_capacity.try_into().unwrap());
+        FloatingImage {
+            width,
+            height,
+            data: buffer,
+            name,
+        }
+    }
+    fn set_data(&mut self, data: Vec<u8>) -> Result<(), ImageDataErrors> {
+        if data.len() > self.data.capacity() {
+            return Err(ImageDataErrors::BufferTooSmall);
+        }
+        self.data = data;
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), ImageDataErrors> {
+    let args = Args::new();
+    let (image_1, image_format_1) = find_image_from_path(args.image_1)?;
+    let (image_2, image_format_2) = find_image_from_path(args.image_2)?;
+    if image_format_1 != image_format_2
+    {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+
+    let (image_1, image_2) = standardize_size(image_1, image_2);
+    let mut output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+    let combine_data = combine_images(image_1, image_2);
+    output.set_data(combine_data)?;
+
+    if let Err(e) = image::save_buffer_with_format(output.name, &output.data, output.width, output.height, image::ColorType::Rgba8, image_format_1) {
+        return Err(ImageDataErrors::UnableToSaveImage(e))
+    }
+
+    Ok(())
+}
+
+fn find_image_from_path(path: String) -> Result<(DynamicImage, ImageFormat), ImageDataErrors> {
+    match Reader::open(&path) {
+        Ok(image_reader) => {
+            if let Some(image_format) = image_reader.format(){
+                match image_reader.decode() {
+                    Ok(image)=> Ok((image, image_format)),
+                    Err(e)=> Err(ImageDataErrors::UnableToDecodeImage(e))
+                }
+
+            }else {
+                return Err(ImageDataErrors::UnableToFormatImage(path))
+            }
+        }
+        Err(e) => Err(ImageDataErrors::UnableToReadFromPath(e))
+    }
+}
+
+fn standardize_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, hieght) = get_smallest_dimension(image_1.dimensions(), image_2.dimensions());
+    println!("Width: {}, Height: {}", width, hieght);
+
+    if image_2.dimensions() == (width, hieght) {
+        (image_1.resize_exact(width, hieght, Triangle), image_2)
+    } else {
+        (image_1, image_2.resize_exact(width, hieght, Triangle))
+    }
+}
+
+fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pic_1 = dim_1.0 * dim_1.1;
+    let pic_2 = dim_1.0 * dim_1.1;
+    return if pic_1 < pic_2 { dim_1 } else { dim_2 };
+}
+
+fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+    let vec_1 = image_1.to_rgba8().into_vec();
+    let vec_2 = image_2.to_rgba8().into_vec();
+
+    alternate_pixels(vec_1, vec_2)
+}
+
+fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    let mut combine_data = vec![0u8; vec_1.len()];
+    let mut i = 0;
+    while i < vec_1.len() {
+        if i % 8 == 0 {
+            combine_data.splice(i..=i + 3, set_rgba(&vec_1, i, i + 3));
+        } else {
+            combine_data.splice(i..=i + 3, set_rgba(&vec_2, i, i + 3));
+        }
+        i += 4;
+    }
+    combine_data
+}
+
+fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+    let mut rgba = Vec::new();
+    for i in start..=end {
+        let val = match vec.get(i) {
+            Some(d) => *d,
+            None => panic!("value Out Of Bounds")
+        };
+        rgba.push(val);
+    }
+    rgba
+}
